591675429a769aeceee12bbac84eb60c0c05315a
DSilva21 <31378533+DSilva21@users.noreply.github.com>
2017-10-24 20:16:53 +0900

    Added delete tree function

--- BinaryTree.c
+++ BinaryTree.c
@@ -41,3 +41,17 @@ void MakeRightSubTree(BTreeNode*main, BTreeNode*sub)
 		free(main->right);
 	main->right = sub;
 }
+
+
+void DeleteTree(BTreeNode *bt)
+{
+	if (bt == NULL)
+		return; 
+	
+	DeleteTree(bt->left);
+	DeleteTree(bt->right);
+
+	
+	printf("del tree data: %d\n", bt->data);
+	free(bt);
+}
